a33b0a4c2c4a1eee66082d6fce9469adefb15378
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2020-07-21 23:42:03 +0200

    🔍

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,114 @@
 import React, { useEffect, useState, useRef } from "react";
+import _, { debounce } from "lodash";
 import { stringifyUrl } from "query-string";
 import { useQuery } from "react-query";
 import Measure from "react-measure";
 import Graph from "./Graph";
 import "./App.css";
 
+type Result = {
+  title: string;
+};
+
 const Input = ({ width = 500, height = 75 }) => {
   const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState([] as Result[]);
   const inputRef = useRef<HTMLInputElement>(null);
+  const debouncedAutocomplete = useRef(
+    _.debounce(async (input: string) => {
+      setLoading(true);
+
+      try {
+        let url = stringifyUrl({
+          url: "http://localhost:4004/api/results",
+          query: {
+            input,
+          },
+        });
+
+        let res = await fetch(url, { method: "GET" });
+        let json = await res.json();
+        setResults(json.results);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }, 1000)
+  );
 
   return (
-    <div className="search" style={{ maxWidth: width, height }}>
-      <input
-        ref={inputRef}
-        type="text"
-        value={value}
-        placeholder="Search!"
-        style={{ fontSize: 24 }}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
-      {value.length > 0 && (
-        <button
-          onClick={() => {
-            setValue("");
-            inputRef.current?.focus();
-          }}
-        >
-          <i className="icon ion-android-close" />
-        </button>
-      )}
-      {loading ? (
-        <button disabled={true}>
-          <div className="loader" />
-        </button>
-      ) : (
-        <button
-          onClick={(event) => {
-            event.preventDefault();
+    <div style={{ position: "relative", width }}>
+      <div className="search" style={{ maxWidth: width, height }}>
+        <input
+          ref={inputRef}
+          type="text"
+          value={value}
+          placeholder="Search!"
+          style={{ fontSize: 24 }}
+          onChange={({ target: { value } }) => {
+            setValue(value);
 
-            setLoading(true);
-
-            if (value.length > 0) {
+            if (value.length === 0) {
+              debouncedAutocomplete.current.cancel();
+              setResults([]);
+            } else {
+              debouncedAutocomplete.current(value);
             }
           }}
-        >
-          <i className="icon ion-android-search" />
-        </button>
-      )}
+        />
+        {value.length > 0 && (
+          <button
+            onClick={() => {
+              setValue("");
+              setResults([]);
+              debouncedAutocomplete.current.cancel();
+              inputRef.current?.focus();
+            }}
+          >
+            <i className="icon ion-android-close" />
+          </button>
+        )}
+        {loading ? (
+          <button disabled={true}>
+            <div className="loader" />
+          </button>
+        ) : (
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+
+              if (value.length > 0) {
+                debouncedAutocomplete.current.flush();
+              }
+            }}
+          >
+            <i className="icon ion-android-search" />
+          </button>
+        )}
+      </div>
+      <div
+        className="results"
+        style={{
+          zIndex: 999,
+          position: "absolute",
+          top: "100%",
+          width,
+          overflow: "hidden",
+        }}
+      >
+        {results.map((result) => (
+          <div key={result.title} className="result">
+            {result.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 const App = () => {
-  useEffect(() => {
-    let fetchData = async () => {
-      let url = stringifyUrl({
-        url: "http://localhost:4004/api/evaluate",
-        query: {
-          expr: "Albert Einstein",
-        },
-      });
-
-      let res = await fetch(url, {
-        method: "GET",
-      });
-
-      let json = await res.json();
-
-      console.log("json", json);
-    };
-
-    fetchData();
-  }, []);
   return (
     <div
       style={{
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import AcademicApi from "./api";
 import api_key from "./api_key";
 
 const port = 4004;
-const endpoint = "https://api.labs.cognitive.microsoft.com/academic/v1.0";
+const endpoint = "https://api.labs.cognitive.microsoft.com/academic/v1.0/";
 const root = "/api";
 
 const app = express();
@@ -17,36 +17,62 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// const api = new AcademicApi({ api_key, endpoint });
+const postify = (parameters: object) =>
+  Object.entries(parameters)
+    .map(
+      ([key, value]: [string, string | number]) =>
+        `${key}=${value.toString().split(" ").join("%20")}`
+    )
+    .join("&");
 
 app.get(`${root}/evaluate`, async (req, res) => {
-  let parameters = parseUrl(req.originalUrl);
-  let query = _.pick(parameters.query, [
-    "expr",
-    "model",
-    "count",
-    "offset",
-    "orderBy",
-    "attributes",
-  ]);
+  let query = _.chain(req.query)
+    .pick(["expr", "model", "count", "offset", "orderBy", "attributes"])
+    .mapValues((value) => value?.toString())
+    .value();
 
-  console.log("query", query);
+  let url = stringifyUrl({ url: endpoint + "evaluate?", query });
 
-  if (!query.expr) {
-    throw new Error();
-  }
-
-  let url = stringifyUrl({ url: endpoint, query });
   let response = await fetch(url, {
     method: "GET",
     headers: {
       "Ocp-Apim-Subscription-Key": api_key,
     },
   });
-  res.send(response);
+
+  res.json(await response.json());
 });
 
-app.get(`${root}/interpret`, async (req, res) => {});
+app.get(`${root}/results`, async (req, res) => {
+  const input = req.query.input?.toString()?.toLowerCase();
+
+  let query = _.mapValues(
+    {
+      expr: `And(Ty='0', Or(Ti='${input}', Composite(AA.AuN='${input}')))`,
+      count: 5,
+      attributes: "Ti,Ty,AA.AuN,CC",
+      // orderby: "CC:desc",
+    },
+    (value) => value?.toString()
+  );
+
+  let url = stringifyUrl({ url: endpoint + "evaluate?", query });
+
+  let response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Ocp-Apim-Subscription-Key": api_key,
+    },
+  });
+
+  let json = await response.json();
+
+  res.json({
+    results: json.entities.map(({ Ti }: { Ti: string }) => ({
+      title: Ti,
+    })),
+  });
+});
 
 app.listen(port, async () => {
   console.log(`Example app listening at http://localhost:${port}`);
